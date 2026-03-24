6ec8f309fdae7b06eff6a58534a03aeb54f3b585
MafiaBV1 <123456789+MafiaBV1@users.noreply.github.com>
2026-03-24 14:00:02 +0100

    Dataloader.java

--- src/DataLoader.java
+++ src/DataLoader.java
@@ -1,13 +1,27 @@
+
+/**
+ * WineDataLoader
+ *
+ * Provides the unique alcahole content values extrcted from the combined
+ * red-wine (winequality-red.csv) and white-wine (winequality-white.csv) dataset
+ *
+ *
+ * Dataset source: https://archive.ics.uci.edu/ml/datasets/wine+quality
+ *
+ * Total unique values 112
+ */
+
 public class DataLoader {
+
     /**
-     * WineDataLoader
-     *
-     * Provides the unique alcahole content values extrcted from the combined
-     * red-wine (winequality-red.csv) and white-wine (winequality-white.csv) dataset
-     *
-     *
-     *
-     * åoihgew oihfeP
+     * Return a fresh copy of the 112 unique alcohole content values
+     * (combined from red and withe wine datasets),sorted by value
      */
 
+    public static double[] getUniqueAlcoholValues() {
+        return new double[] {
+                8.0, 8.0, 8.4, 8.5, 8.5, 8.7, 8.8, 8.9,
+
+        };
+    }
 }
